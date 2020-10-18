674d88f2698b9df9a75986c8dad89a9d1502434e
Chadin Anuwattanaporn <chadin.anuwattanaporn@gmail.com>
2020-10-18 17:29:30 +0800

    Fix typo in test cases Add validation to disallow booking on top of another Update user fixture

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,18 @@
 class Booking < ApplicationRecord
   belongs_to :venue
   belongs_to :user
+
+  validate :booking_cannot_be_on_top_of_another
+
+  def booking_cannot_be_on_top_of_another
+    # find bookings of same venue
+    # end date in between current booking interval
+    # start date in between current booking interval
+    # start is before start, end is after end
+    conflicting_bookings = Booking.where("venue_id = :venue_id and ((\"from\" <= :from and \"to\" > :from) or (\"from\" <= :to and \"to\" > :to))", 
+    {venue_id: venue.id, from: from, to: to})
+    if conflicting_bookings.length > 0
+      errors.add(:from, "The booking timing conflicts with an existing booking")
+    end
+  end
 end
--- test/models/booking_test.rb
+++ test/models/booking_test.rb
@@ -12,10 +12,26 @@ class BookingTest < ActiveSupport::TestCase
     booking = Booking.new
     booking.venue = venue
     booking.user = user
-    booking.from = DateTime.civil_from_format :utc, 2020, 10, 17, 11
-    booking.to = DateTime.civil_from_format :utc, 2020, 10, 17, 12
+    booking.from = DateTime.civil_from_format :utc, 2020, 10, 17, 12
+    booking.to = DateTime.civil_from_format :utc, 2020, 10, 17, 13
 
-    assert venue.save
+    assert booking.save
+
+    venue.reload
+    assert venue.bookings.length > 2
+  end
+
+  test "should be able to make a booking timing next to each other" do
+    venue = Venue.find_by(name: 'Meeting Room 1')
+    user = User.find_by(username: 'one')
+    booking = Booking.new
+    booking.venue = venue
+    booking.user = user
+    #there is already a booking 12-1pm, but 12-1pm + 1-2pm should be OK
+    booking.from = DateTime.civil_from_format :utc, 2020, 10, 17, 13
+    booking.to = DateTime.civil_from_format :utc, 2020, 10, 17, 14
+
+    assert booking.save
   end
 
   test "should not be able to make a booking on top of existing booking" do
@@ -27,6 +43,6 @@ class BookingTest < ActiveSupport::TestCase
     booking.from = DateTime.civil_from_format :utc, 2020, 10, 17, 9
     booking.to = DateTime.civil_from_format :utc, 2020, 10, 17, 10
 
-    assert_not venue.save
+    assert_not booking.save
   end
 end
