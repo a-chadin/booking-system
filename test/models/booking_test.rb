bd0dd56eb26dbfe5d5abaed4110284f70e3854da
Chadin Anuwattanaporn <chadin.anuwattanaporn@gmail.com>
2020-10-18 16:03:10 +0800

    Add base test cases Able to book Not able to book on top of each other

--- test/models/booking_test.rb
+++ test/models/booking_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
 
 class BookingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should be able to retrive current bookings" do
+    venue = Venue.find_by(name: 'Meeting Room 1')
+    assert venue.bookings.length == 2
+  end
+
+  test "should be able to make a booking" do
+    venue = Venue.find_by(name: 'Meeting Room 1')
+    user = User.find_by(username: 'one')
+    booking = Booking.new
+    booking.venue = venue
+    booking.user = user
+    booking.from = DateTime.civil_from_format :utc, 2020, 10, 17, 11
+    booking.to = DateTime.civil_from_format :utc, 2020, 10, 17, 12
+
+    assert venue.save
+  end
+
+  test "should not be able to make a booking on top of existing booking" do
+    venue = Venue.find_by(name: 'Meeting Room 1')
+    user = User.find_by(username: 'one')
+    booking = Booking.new
+    booking.venue = venue
+    booking.user = user
+    booking.from = DateTime.civil_from_format :utc, 2020, 10, 17, 9
+    booking.to = DateTime.civil_from_format :utc, 2020, 10, 17, 10
+
+    assert_not venue.save
+  end
 end
