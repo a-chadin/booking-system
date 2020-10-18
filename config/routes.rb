702593512a2ec39413b6642a42b05dcfd55bc530
Chadin Anuwattanaporn <chadin.anuwattanaporn@gmail.com>
2020-10-18 18:54:33 +0800

    add routes add venue controller implement simple views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :venues, only: [:index, :show] do
+    resources :bookings, only: [:new, :create, :update, :edit, :destroy]
+  end
 end
