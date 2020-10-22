aa8683f411b6f00795a5b831841a0c0b76e2c415
Chadin Anuwattanaporn <chadin.anuwattanaporn@gmail.com>
2020-10-22 19:31:53 +0800

    update booking model to handle edge cases correctly

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -9,7 +9,7 @@ class Booking < ApplicationRecord
     # end date in between current booking interval
     # start date in between current booking interval
     # start is before start, end is after end
-    conflicting_bookings = Booking.where("venue_id = :venue_id and ((\"from\" <= :from and \"to\" > :from) or (\"from\" <= :to and \"to\" > :to))", 
+    conflicting_bookings = Booking.where("venue_id = :venue_id and ((\"from\" >= :from and \"from\" < :to) or (\"to\" > :from and \"to\" <= :to) or (\"from\" <= :from and \"to\" >= :to))", 
     {venue_id: venue.id, from: from, to: to})
     if conflicting_bookings.length > 0
       errors.add(:from, "The booking timing conflicts with an existing booking")
