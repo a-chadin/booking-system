efb62b228d99336cb0ff8e1980a2a179747536b8
Chadin Anuwattanaporn <chadin.anuwattanaporn@gmail.com>
2020-10-22 19:30:15 +0800

    update session controller add helper to get current user

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -2,18 +2,18 @@ require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get sessions_new_url
+    get login_url
     assert_response :success
   end
 
   test "should get create" do
-    get sessions_create_url
+    post login_url
     assert_response :success
   end
 
   test "should get destroy" do
-    get sessions_destroy_url
-    assert_response :success
+    get logout_url
+    assert_response :found
   end
 
 end
